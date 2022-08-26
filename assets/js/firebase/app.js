ae738d1cffd82fcaa054e185f7cf6368f4dcb1f1
Gabitob <gabitob@live.com>
2022-08-26 14:11:16 -0300

    segundo dia

---
+++ assets/js/firebase/app.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCB40VI4njmq_dkEV7XdXu6634dA3pWurg",
+    authDomain: "projeto-semana-front-end-8644c.firebaseapp.com",
+    projectId: "projeto-semana-front-end-8644c",
+    storageBucket: "projeto-semana-front-end-8644c.appspot.com",
+    messagingSenderId: "385138292517",
+    appId: "1:385138292517:web:bf602da06fd089b0ff8ccc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app
---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+import { subscribeToHellFireClub } from "./firebase/hellfire-club.js"
+
+const txtName = document.getElementById("txtName")
+const txtEmail = document.getElementById('txtEmail')
+const txtLevel = document.getElementById('txtLevel')
+const txtCharacter = document.getElementById('txtCharacter')
+
+const btnSubscribe = document.getElementById('btnSubscribe')
+
+btnSubscribe.addEventListener("click", async () => {
+    const subscription = {
+        name: txtName.value,
+        email: txtEmail.value,
+        level: txtLevel.value,
+        character: txtCharacter.value
+    }
+
+    const subscriptionId = await subscribeToHellFireClub(subscription)
+    console.log(`Inscrito com sucesso: ${subscriptionId}`)
+
+    txtName.value = ''
+    txtEmail.value = ''
+    txtLevel.value = ''
+    txtCharacter.value = ''
+})
